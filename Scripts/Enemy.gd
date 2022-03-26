f1893195e215f3b93b42550a3834d3f682b70ceb
Johannes Elgaard <joha4270@gmail.com>
2022-03-26 18:55:13 +0100

    Made Ants move semi-randomly

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -8,11 +8,26 @@ var start_pos: Vector2
 var target_pos: Vector2
 var dead = false
 export var speed: float
+export (int) var position_offset_stages = 2
+export (Vector2) var position_offset = Vector2(32, 32)
+var rng = RandomNumberGenerator.new()
 
 const Nodes := preload("res://Scripts/Nodes.gd")
 var _ref_Nodes: Nodes
 
 var path : PoolVector2Array
+
+func random_vector(maxSize : Vector2):
+	return Vector2(rng.randf_range(maxSize.x / -2, maxSize.x / 2), rng.randf_range(maxSize.y / -2, maxSize.y / 2))
+
+func mess_path(points : PoolVector2Array):
+	var state = random_vector(position_offset)
+	print("Messing points")
+	for i in range(points.size()):
+		print("   Offset state of " + str(state))
+		points[i] += state
+		state = (state * (position_offset_stages - 1) +  random_vector(position_offset)) / position_offset_stages
+	return points
 
 func set_route(points : PoolVector2Array):
 	path = points
@@ -23,12 +38,13 @@ func pathfind(astar : AStar2D):
 	if route.empty():
 		print("Enemy unable to find path. This should never happen")
 		route.push_back(target_pos)
-	set_route(route)
+	set_route(mess_path(route))
 
 func _ready():
 	add_to_group(_Groups.ENEMY)
 	_ref_Nodes = get_node("/root/MainScene/Nodes")
 	_ref_Nodes.add_node(self)
+	rng.randomize()
 	
 func _process(delta):
 	if not initialized:
